195603ef6ac9824827ecd36afd0c3179db3ea1a1
Craig Tingey <tingeycraig@gmail.com>
2015-12-07 12:33:35 -0700

    nothing really

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from filehandler import FileHandler
 
 
-# filename = 'Puzzle-4x4-0001.txt'
+filename = 'Puzzle-9x9-0305.txt'
 # filename = 'Puzzle-16x16-0001-Inconsistant.txt'
-filename = 'Puzzle-9x9-0101.txt'
+# filename = 'Puzzle-25x25-0001-Inconsistant.txt'
+# filename = 'Puzzle-9x9-0101.txt'
 # filename = 'Puzzle-9x9-0203.txt'
 # filename = 'Puzzle-9x9-0000-Unsolvable.txt'
 
